1b0b36461b5864a5a6cb2733c593a8cc751f59a1
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2021-02-22 22:35:06 +0200

    Delete what is unnecessary

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -5,35 +5,37 @@ import Footer from './Footer';
 import UploadCard from './UploadCard';
 import Loader from './Loader';
 import LoadedCard from './LoadedCard';
+import Message from './Message';
 import './App.css';
 
 const App = () => {
   const [imageUrl, setImageUrl] = useState('');
   const [isUploading, setIsUploading] = useState(false);
+  const [message, setMessage] = useState('');
+  const location = 'http://localhost:3000';
 
-  const upload = (formData) => {
-    setIsUploading(true);
-    axios.post('/api/upload-image', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      onUploadProgress: progressEvent => console.log(progressEvent.loaded)
-    }).then(
-      (res) => {
-        setImageUrl(`http://localhost:3000/images/${res.data.data.filename}`);
-        setIsUploading(false);
-      },
-      (err) => {
-        console.log(err);
-        setIsUploading(false);
-      },
-    )
+  const upload = async (formData) => {
+    try {
+      setIsUploading(true);
+      const result = await axios.post('/api/upload-image', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: progressEvent => console.log(progressEvent.loaded),
+      });
+      setImageUrl(`${location}/images/${result.data.data.filename}`);
+    } catch (err) {
+      setMessage('Something went wrong!')
+    } finally {
+      setIsUploading(false);
+    }
   }
 
   return (<div className="main">
     {(!isUploading && !imageUrl) && <UploadCard cb={upload} />}
     {isUploading && <Loader />}
     {(!isUploading && imageUrl) && <LoadedCard imgUrl={imageUrl} />}
+    {message && <Message>{{message}}</Message>}
     <Footer />
   </div>)
 }
