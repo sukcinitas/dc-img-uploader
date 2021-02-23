ba039a4f99e8bccc35484b7961f8ff34baaced51
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2021-02-23 15:09:30 +0200

    Change div into img so that image itself would be copyable

--- src/client/LoadedCard.jsx
+++ src/client/LoadedCard.jsx
@@ -5,6 +5,7 @@ import Message from './Message';
 const LoadedCard = ({ imgUrl }) => {
   const [message, setMessage] = useState('');
   const imgUrlRef = useRef(null);
+  const imgRef = useRef(null);
 
   const clipText = () => {
     if (!navigator.clipboard) {
@@ -36,10 +37,7 @@ const LoadedCard = ({ imgUrl }) => {
         <span className="card__icon material-icons">check_circle</span>
         <h2 className="card__heading">Uploaded Successfully!</h2>
       </header>
-      <div 
-        style={{backgroundImage: `url(${imgUrl})`}}
-        className="card__box card__box--img" 
-      />
+      <img className="card__box card__box--img" src={imgUrl} />
       <div className="card__link-wrapper">
         <p className="card__link" ref={imgUrlRef}>{imgUrl}</p>
         <button size="small" className="button button--small" onClick={clipText}>Copy Link</button>
