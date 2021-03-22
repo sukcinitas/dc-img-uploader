be7dc72ed3fe55350a460db1bb1fd4114229c973
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2021-03-22 12:44:25 +0200

    Remove unused dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -28,7 +28,7 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(js|jsx)$/,
