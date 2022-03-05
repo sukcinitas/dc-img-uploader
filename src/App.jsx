e2a26ea9dbec07eee60b1d8b7dd238ea97cbfd38
Gintare Cerniauskaite <gintare.cerniauskaite@protonmail.com>
2022-03-05 22:17:51 +0200

    Tidy logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,49 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-import Footer from "./Footer";
-import UploadCard from "./UploadCard";
-import Loader from "./Loader";
-import LoadedCard from "./LoadedCard";
-import Message from "./Message";
-import "./App.css";
+import Footer from './Footer';
+import UploadCard from './UploadCard';
+import Loader from './Loader';
+import LoadedCard from './LoadedCard';
+import Message from './Message';
+import './App.css';
 
 const App = () => {
-  const [imageUrl, setImageUrl] = useState("");
+  const [imageUrl, setImageUrl] = useState('');
   const [isUploading, setIsUploading] = useState(false);
   const [isImgLoaded, setIsImgLoaded] = useState(false);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
   const location =
-    window.location.href === "http://localhost:8080/"
-      ? "http://localhost:3000/"
-      : "https://boiling-citadel-16368.herokuapp.com/";
+    window.location.href === 'http://localhost:8080/'
+      ? 'http://localhost:3000/'
+      : 'https://boiling-citadel-16368.herokuapp.com/';
 
   const upload = async (formData) => {
     try {
       setIsUploading(true);
       const result = await axios.post(`${location}api/upload-image`, formData, {
         headers: {
-          "Content-Type": "multipart/form-data",
+          'Content-Type': 'multipart/form-data',
         },
         onUploadProgress: (progressEvent) => console.log(progressEvent.loaded),
       });
       setImageUrl(`${location}api/images/${result.data.data.filename}`);
     } catch (err) {
-      if (err.response && err.response.status === 400) {
-        setMessage("Only jpeg, png or gif files can be uploaded!");
-      } else {
-        setMessage("Something went wrong!");
-      }
-      formData.delete("image");
+      setMessage(
+        err.response && err.response.status === 400
+          ? 'Only jpeg, png or gif files can be uploaded!'
+          : 'Something went wrong!'
+      );
+      formData.delete('image');
     } finally {
       setIsUploading(false);
       setTimeout(() => {
-        setMessage("");
+        setMessage('');
       }, 1500);
     }
   };
 
-  let value;
+  let value = <UploadCard cb={upload} />;
   if (isUploading || imageUrl) {
     value = (
       <>
@@ -55,11 +55,7 @@ const App = () => {
         />
       </>
     );
-  } else {
-    value = <UploadCard cb={upload} />;
   }
-
-  console.log(isUploading || imageUrl, isUploading || !isImgLoaded, value);
 
   return (
     <div className="main">
